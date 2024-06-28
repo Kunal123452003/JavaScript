cddd7dc3d47e3c9fdaee41cba8d56f513641deeb
Kunal123452003 <117072808+Kunal123452003@users.noreply.github.com>
2024-06-28 09:09:49 +0000

    printing using console.table()

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,6 +1,19 @@
 const accId = 144553
 let accEmail = "Kunal123452003@gmail.com"
 var accPassword = "12345"
-accCity = "Delhi"
+accCity = "Noida"
 
 //   accId = 23  // As AccID is  constant variable it cant be changed
+
+ accEmail = "user@gmail.com"
+ accPassword = "2003"
+ accCity = "Jahangir puri"
+
+ // Printing using console.log()
+ console.log("Acc ID",accId)
+ console.log("Acc Email",accEmail)
+ console.log("Acc Password",accPassword)
+ console.log("Acc city",accCity)
+
+ // Printing data in a tabular form
+console.table([accId,accEmail,accPassword,accCity])
