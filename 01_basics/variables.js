98de171c25db2dbe7cf2da7ae5f7bde7bc1ed8d3
Kunal123452003 <117072808+Kunal123452003@users.noreply.github.com>
2024-06-28 09:28:02 +0000

    Diff btw let, var, const

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,7 +1,10 @@
 const accId = 144553
 let accEmail = "Kunal123452003@gmail.com"
 var accPassword = "12345"
+//   accCity      // here value would not be undefined, it will show error: "accCity is not defined"
 accCity = "Noida"
+
+let accState       // its value willbe undefined as there is no value is defined here 
 
 //   accId = 23  // As AccID is  constant variable it cant be changed
 
@@ -16,4 +19,13 @@ accCity = "Noida"
  console.log("Acc city",accCity)
 
  // Printing data in a tabular form
-console.table([accId,accEmail,accPassword,accCity])
+console.table([accId,accEmail,accPassword,accCity,accState])
+
+/*
+Difference btw the var and let is of scope 
+in early verions of js var is used to declare a variable and when same variable's value is changed a program 
+it got changed for all the places in a program   
+because there is no concept of scope{} 
+so to tackle that problem let is used now 
+AVOID use of var 
+*/
