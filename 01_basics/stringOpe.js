c6955a51fb96853eb5863ab6ab1732dfe57e737d
Kunal123452003 <117072808+Kunal123452003@users.noreply.github.com>
2024-07-01 06:04:58 +0000

    Done

---
+++ 01_basics/stringOpe.js
@@ -0,0 +1,53 @@
+// Normal string
+let a = "Kunal"
+console.log(a)
+
+// Normal arithematic operations
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%2)
+console.log(2**3)
+
+// Normal string operations
+let b = 'ABC'
+let c = "DEF"
+console.log(b + c)
+
+// String + Number
+console.log("1" + 2)    //Number + string = string
+//Number + string
+console.log(1 + "2")
+
+// what happens when 
+// String + Number + Number
+console.log("1" + 2 + 2)    // Output: 122  when "1"(String) + 2(Number) it becomes "12"(String) then "12"(String) + 2(Number) it becomes "122"(String)
+                            // it is guideline and rule in js that when a Number + string = string
+
+console.log(1 + 2 + "2")    // Output: 32 when 1(Number) + 2(Number) = 3(Number) then 3(Number) + "2"(String) it becomes "32"(String)
+
+// Prefix
+let x = 3
+let y = x++          
+console.log("x:", x , "y:",y)
+/* Output:  x: 4 y: 3    
+first assign value to y then increment it
+its like 
+  x = 3 
+  y = x
+  x++
+*/
+
+//Postfix
+let d = 4
+let e =++d
+console.log("d:",d,"e:",e)
+/*
+Output:
+d: 5 e: 5
+first increment then assign value to e
+ d = 4
+ ++d  or d++
+ e = d
+*/
