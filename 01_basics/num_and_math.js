87ba09d886bdf577d00f26c3163d980b765e1e16
Kunal123452003 <117072808+Kunal123452003@users.noreply.github.com>
2024-07-02 08:52:45 +0000

    done

---
+++ 01_basics/num_and_math.js
@@ -0,0 +1,37 @@
+// const num1 = new Number(1004456)
+// console.log(num1)
+
+// // toString() used to convert number to a string 
+// console.log(num1.toString().length)
+
+// const num2 = 23.845632
+// console.log(num2)
+
+// console.log(num2.toFixed(3))    // .toFixed() is used to set the number of values after point
+
+// const num3 = 2823.845632
+// console.log(num3.toPrecision(5))  // this function is used to give the pricise value to the number 
+
+// const num4 = 1000000
+// console.log(num4.toLocaleString())     // representing number acc to foreign Standard
+// console.log(num4.toLocaleString('en-IN'))  // representing numner acc to India Standards
+
+// // Number class properties
+// console.log(Number.EPSILON)
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_VALUE)
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(Number.MIN_SAFE_INTEGER)
+
+//-----------Math---------------
+console.log(Math)
+console.log(Math.abs(-3))   // abs used to convert nehative values to positive only
+console.log(Math.log(4))
+console.log(Math.ceil(4.335)) //Max possible value of 4.335 is 5
+console.log(Math.floor(4.335)) //Min possible value of 4.335 is 4
+console.log(Math.max(1,2,3,4))
+console.log(Math.min(1,2,3.8,4))
+
+console.log(Math.random())   //gives random value from 0 to 1
+
+console.log((Math.random()*10)+1)  //generatting numbers from 1 to 10
