501c75a0f4753e55aaaee8618bf18b9ec5101747
Kunal <117072808+Kunal123452003@users.noreply.github.com>
2024-07-07 10:38:46 +0000

    this done

--- 02_basic/this_and_arrow.js
+++ 02_basic/this_and_arrow.js
@@ -4,11 +4,14 @@ const obj1 = {
 
     greet: function(){
         console.log(`Hello ${this.name}`);
+    },
+    show : function(){
+        console.log(this)
     }
 }
 
 // console.log(obj1.greet())  Run
-// obj1.greet()               Run
+obj1.greet()                //Run
 
 console.log(this)
 /*
@@ -23,4 +26,53 @@ When you run console.log(this) in the global scope of a browser environment, thi
 
 In Node.js
 When you run console.log(this) in the global scope of a Node.js module, this refers to the module's exports, which is an empty object {} by default.
+*/
+
+function run(){
+    console.log(this)
+}
+
+run()
+
+//  arrow function
+const Run = () => {
+    console.log(`This is arrow function`)
+}
+
+Run()
+
+const a = () => {
+    name = "Kunal"
+    console.log(this);
+}
+a()          // in arrow function this keyowrd cant be used
+obj1.show()  // in normal member function this keyword shows the properties of obj1
+run()         // also this keyword also show information of global object 
+
+// Arrow function
+
+const add = (num1,num2) => {
+    return num1 + num2
+}
+console.log(add(1,3))
+
+//Similarly when their is only one of statement/code 
+//in the function we can write is as
+const addTwo = (num1,num2) => num1+num2
+console.log(addTwo(1,3))
+
+// also we can use () parenthesis
+const addition = (num1,num2) => (num1+num2)
+console.log(addition(1,3))
+
+// returning object  needs () parenthesis
+const obj_return = () => ({ name :"Kunal",age:21})
+console.log(obj_return())
+
+//Summary 
+/*
+   function () {}     may use return keyword or may not
+   () => {}           arrow function may use return keyword may not
+   () =>              dont uses return keyword
+   () = > ()          used when we return object and also dont use return keyword  
 */
